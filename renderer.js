8d4b593a136dfcba53447b524355a578fd0b5e18
RoySegall <roy.segall@gizra.com>
2016-09-13 08:58:53 +0300

    Init

--- renderer.js
+++ renderer.js
@@ -1,3 +1,27 @@
 // This file is required by the index.html file and will
 // be executed in the renderer process for that window.
 // All of the Node.js APIs are available in this process.
+
+$(function(){
+
+  var r = require('rethinkdb');
+
+  var connection = null;
+  r.connect( {host: 'localhost', port: 28015}, function(err, conn) {
+    
+    if (err) {
+      throw err;
+    }
+
+    connection = conn;
+
+    r.db('test').table('names').changes().run(conn, function(err, cursor) {
+      cursor.each(function(connection, value) {
+
+        $('body').append(value['new_val'].message + "<br />");
+      });
+    })
+  });
+
+});
+
