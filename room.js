69bffb1a9994a0167981247af7522945b23e3d1c
Roy Segall <roy.segall@gizra.com>
2016-09-17 23:15:48 +0300

    Done!

--- room.js
+++ room.js
@@ -2,6 +2,8 @@ const $ = require('jquery');
 const queryString = require('query-string');
 const queryArguments = queryString.parse(location.search);
 
+window.$ = window.jQuery = $;
+
 $(function() {
 
   var
@@ -22,7 +24,20 @@ $(function() {
     db.table('interest_room_messages').filter({'room_id': queryArguments['id']}).changes().run(conn, function(err, cursor) {
       $('.dimmer').hide();
 
-      cursor.each(console.log);
+      cursor.each(function(connection, value) {
+        $('.body').append("<div class='message'>" + value['new_val'].text + "</div>");
+      });
+    });
+
+    $('.ui.submit.button').click(function() {
+      db.table('interest_room_messages').insert({
+        'created': parseInt(Math.floor(new Date().getTime() / 1000)),
+        'room_id': queryArguments['id'],
+        'text': $('#message').val(),
+        'user_id': 1 // for now.
+      }).run(conn, function(err, cursor) {
+        $('#message').val('');
+      });
     });
   });
 
